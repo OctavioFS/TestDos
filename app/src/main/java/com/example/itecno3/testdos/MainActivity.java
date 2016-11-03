872f43c03736d45019fb509cc66e740c47c55848
itecno3 <olabastida@tecnoliza.me>
2016-11-03 12:39:22 -0600

    este es de octavio

--- app/src/main/java/com/example/itecno3/testdos/MainActivity.java
+++ app/src/main/java/com/example/itecno3/testdos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.itecno3.testdos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String simon = "esto es una pueba ...";
+        //Toast.makeText(getApplicationContext(),simon, )
     }
 }
