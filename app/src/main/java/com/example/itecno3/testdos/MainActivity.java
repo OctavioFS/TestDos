7f89be012da2c43d6cc4dd0620e731ded5cbc4bb
itecno3 <olabastida@tecnoliza.me>
2016-11-03 12:27:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/itecno3/testdos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.itecno3.testdos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String simon = "esto es una pueba ...";
+    }
+}
